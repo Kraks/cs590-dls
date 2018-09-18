4cbece97241698019293f9eb8f2713b3b2e551c4
Guannan Wei <guannanwei@outlook.com>
2018-09-17 23:30:29 -0400

    init

---
+++ src/main/scala/sat/Hello.scala
@@ -0,0 +1,113 @@
+package sat.dpll
+
+import math._
+
+object CNF {
+  type Assgn = Map[Int, Boolean]
+
+  case class Var(x: Int)
+
+  case class Clause(xs: List[Int]) {
+    def size = xs.size
+    def contains(v: Int): Boolean = xs.contains(v)
+    def remove(v: Int): Clause = Clause(xs.filter(_ != v))
+    def containsAny(vs: List[Int]): Boolean = {
+      for (v <- vs) { if (xs.contains(v)) return true }
+      return false
+    }
+    def removeAllOccur(vs: List[Int]): Clause = Clause(xs.filter(!vs.contains(_)))
+
+    def assign(v: Int, b: Boolean): Option[Clause] = {
+      var new_xs = List[Int]()
+      for (x <- xs) {
+        if (abs(x) == abs(v)) { if ((x > 0) == b) return None }
+        else { new_xs = x::new_xs }
+      }
+      Some(Clause(new_xs))
+    }
+  }
+
+  case class Formula(cs: List[Clause]) {
+    def isEmpty: Boolean = cs.isEmpty
+    def containsMtClause: Boolean = cs.contains(Clause(List()))
+
+    private def varsToAssignment(vars: Iterable[Int]) =
+      vars.map((x:Int) => if (x>0) (x→true) else (-x→false)).toMap
+
+    lazy private val unitVars = cs.filter(_.size == 1).map(_.xs.head)
+
+    def containsUnit: Boolean = unitVars.size != 0
+
+    def elimUnit(): (Formula, Assgn) = {
+      val assign = varsToAssignment(unitVars)
+      val result = for { c <- cs if !c.containsAny(unitVars) }
+                   yield c.removeAllOccur(unitVars.map(-_))
+      (Formula(result), assign)
+    }
+
+    lazy private val pureVars = cs.flatMap(_.xs).groupBy(abs).mapValues(_.size).filter({ case (k,v) => v == 1 }).keys.toList
+
+    def containsPure: Boolean = pureVars.size != 0
+
+    def elimPure(): (Formula, Assgn) = {
+      val assign = varsToAssignment(pureVars)
+      val result = for {c <- cs } yield c.removeAllOccur(pureVars)
+      (Formula(result), assign)
+    }
+
+    def pick: Int = cs.head.xs.head
+    def assign(v: Int, b: Boolean): Formula = {
+      Formula((for (c <- cs) yield c.assign(v, b)).filter(_.nonEmpty).map(_.get))
+    }
+  }
+
+  /** (x1 ∨ x2 ∨ -x3 ∨ x6) ∧
+    * (-x2 ∨ x4) ∧
+    * (-x1 ∨ -x5) ∧
+    * (-x1 ∨ x2 ∨ x3 ∨ -x4 ∨ x5) ∧
+    * (-x3) ∧ (x2 ∨ x5) ∧
+    * (-x5) ∧ (x1)
+    */
+  val example_f = Formula(List(Clause(List(1, 2, -3, 6)),
+                               Clause(List(-2, 4)),
+                               Clause(List(-1, -5)),
+                               Clause(List(-1, 2, 3, -4, 5)),
+                               Clause(List(-3)), Clause(List(2, 5)),
+                               Clause(List(-5)), Clause(List(1))))
+}
+
+object DPLL {
+  import CNF._
+
+  def dpll(f: Formula, assgn: Map[Int, Boolean]): Option[Map[Int, Boolean]] = {
+    if (f.containsMtClause) return None
+    if (f.isEmpty) return Some(assgn)
+    if (f.containsUnit) {
+      val (new_f, new_assgn) = f.elimUnit
+      return dpll(new_f, assgn ++ new_assgn)
+    }
+    if (f.containsPure) {
+      val (new_f, new_assgn) = f.elimPure
+      return dpll(new_f, assgn ++ new_assgn)
+    }
+    val v = f.pick
+    val tryTrue = dpll(f.assign(v, true), assgn + (v → true))
+    if (tryTrue.nonEmpty) tryTrue
+    else dpll(f.assign(v, false), assgn + (v → false))
+  }
+
+  def solve(f: Formula): Option[Map[Int, Boolean]] = dpll(f, Map[Int, Boolean]())
+
+}
+
+object Hello extends App {
+  import CNF._
+  import DPLL._
+
+  println("DPLL")
+  println(example_f.elimUnit)
+  println(example_f.elimPure)
+  println(example_f.assign(6, true))
+  println(example_f.assign(6, false))
+  println(solve(example_f))
+}
